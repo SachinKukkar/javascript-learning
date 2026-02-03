f9cdf74931e294f787e6d6789b41a125edcde2e3
Sachin Kukkar <sachinkukkar1998@gmail.com>
2026-02-03 16:12:23 +0530

    complete till 1:38:41

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,26 @@
+let value = 3 
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(4+5);
+// console.log(4-5);
+// console.log(4*5);
+// console.log(4**5);
+// console.log(4/5);
+// console.log(4%5);
+
+let str1 = "hello"
+let str2 = " Sachin"
+
+let str3 = str1+str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+
+// console.log(1+"2");
+
+// console.log("1"+2+2);
+
+// console.log(1+2+"2");
